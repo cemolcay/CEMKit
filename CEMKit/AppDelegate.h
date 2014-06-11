dd249d2e757e7d9eab062510e4243fc48747bd42
cemolcay <ccemolcay@gmail.com>
2014-06-11 15:58:42 +0300

    Initial Commit

---
+++ CEMKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CEMKit
+//
+//  Created by Cem Olcay on 11/06/14.
+//  Copyright (c) 2014 studionord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CEMKitTests/CEMKitTests.m
@@ -0,0 +1,34 @@
+//
+//  CEMKitTests.m
+//  CEMKitTests
+//
+//  Created by Cem Olcay on 11/06/14.
+//  Copyright (c) 2014 studionord. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CEMKitTests : XCTestCase
+
+@end
+
+@implementation CEMKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
